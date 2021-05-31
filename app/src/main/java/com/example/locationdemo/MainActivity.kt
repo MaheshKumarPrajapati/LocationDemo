07c1f7178f3a187f59bf6d918cc72df3c59cb769
Mahesh Prajapati <maheshprajapati1990@gmail.com>
2021-05-31 17:45:48 +0530

    updated ui

--- app/src/main/java/com/example/locationdemo/MainActivity.kt
+++ app/src/main/java/com/example/locationdemo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +21,7 @@ class MainActivity : BaseLocationActivity() {
         findViewById<Button>(R.id.bt_turn_on_gps).setOnClickListener {
             turnOnLocation()
             findViewById<RelativeLayout>(R.id.rl_gps_status).visibility= View.GONE
+            findViewById<ConstraintLayout>(R.id.cl_progressbar).visibility=View.VISIBLE
         }
     }
 
@@ -30,6 +32,7 @@ class MainActivity : BaseLocationActivity() {
 
     override fun updatedLocation(it: Location?) {
         if(it!=null) {
+            findViewById<ConstraintLayout>(R.id.cl_progressbar).visibility=View.GONE
             findViewById<TextView>(R.id.tv_accuracy_value).text = "${it.accuracy}"
             findViewById<TextView>(R.id.tv_latitude_value).text = "${it.latitude}"
             findViewById<TextView>(R.id.tv_longitude_value).text = "${it.longitude}"
